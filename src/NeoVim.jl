ddb8962696cff4bd33c644e1a7dcde25998b5cf1
Sean Marshallsay <srm.1708@gmail.com>
2015-02-08 11:48:19 +0000

    Implement non-async eventloop.

--- src/NeoVim.jl
+++ src/NeoVim.jl
@@ -3,7 +3,7 @@ module NeoVim
 using Compat
 using Docile
 import MsgPack
-export Nvim, NeoVimError, request
+export Nvim, NeoVimError, request, respond
 
 immutable Val{T} end
 
@@ -29,17 +29,56 @@ immutable NeoVimError <: Exception
     id::Int
     msg::UTF8String
 end
-@compat NeoVimError(id, msg::Vector{UInt8}) = NeoVimError(id, UTF8String(msg))
+@compat NeoVimError(errid, msg::Vector{UInt8}) = NeoVimError(errid, bytestring(msg))
 NeoVimError(a::Vector) = NeoVimError(a[1], a[2])
 
+const REQUEST = 0
+const RESPONSE = 1
+const NOTIFICATION = 2
+
+# TODO: make non-bocking
 function request(n::Nvim, func::ByteString, args...)
     requestid = n.reqid
     n.reqid += 1
-    MsgPack.pack(n, Any[0, requestid, func, collect(args)])
+    MsgPack.pack(n, Any[REQUEST, requestid, func, collect(args)])
 
     ret = MsgPack.unpack(n)
     ret[3] === nothing || throw(NeoVimError(ret[3]))
     return sanitize(n, ret[4])
+end
+respond(n::Nvim, reqid, val) = MsgPack.pack(n, Any[RESPONSE, reqid, nothing, val])
+Base.error(n::Nvim, reqid, msg) = MsgPack.pack(n, Any[RESPONSE, reqid, msg, nothing])
+
+function eventloop(nvim::Nvim, data)
+    #@async while true
+    while true
+        msg = MsgPack.unpack(nvim)
+        msgtype = msg[1]
+        if msgtype == REQUEST
+            reqid = msg[2]
+            event = symbol(msg[3])
+            args = sanitize(nvim, msg[4])
+            # TODO: get logging sorted out
+            try
+                #@async request_callback(Val{event}, nvim, reqid, args, data)
+                request_callback(Val{event}, nvim, reqid, args, data)
+            end
+        elseif msgtype == RESPONSE
+            # TODO: not yet async
+        elseif msgtype == NOTIFICATION
+            event = symbol(msg[2])
+            args = sanitize(nvim, msg[3])
+            try
+                notification_callback(Val{event}, nvim, args, data)
+            end
+        else
+            throw(NeoVimError(-1, "Unknown message type."))
+        end
+    end
+end
+
+function request_callback(::Any, nvim, reqid, args, data)
+    error(nvim, reqid, "Unable to handle event.")
 end
 
 end # module
